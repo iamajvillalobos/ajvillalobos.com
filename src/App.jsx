5992c0c85bf7263d64907b28831a6c4c19d1d8ce
AJ Villalobos <hello@ajvillalobos.com>
2024-04-04 23:52:52 +0800

    Initialize site

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <img src="https://ajvillalobos.com/avatar.jpeg" alt="profile-photo" />
+      <p>
+        Hello! I'm AJ Villalobos, a web developer from the Philippines. I
+        specialized in building web apps from scratch and using{" "}
+        <a href="https://rubyonrails.org/" target="_blank">
+          Ruby on Rails Framework
+        </a>
+        .
+      </p>
+
+      <p>
+        I also work on related technologies like React, SQL, HTML5, CSS3,
+        Javascript, Tailwind CSS.
+      </p>
+
+      <p>Related links:</p>
+      <ul>
+        <li>
+          <a href="https://linkedin.com/in/aj-villalobos" target="_blank">
+            Linkedin
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/iamajvillalobos" target="_blank">
+            Github
+          </a>
+        </li>
+      </ul>
+    </main>
+  );
+}
+
+export default App;
