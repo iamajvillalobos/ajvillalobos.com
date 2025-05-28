1333c7dc7667cfa1875e7abf6c3e21edbfd17858
AJ Villalobos <hello@ajvillalobos.com>
2025-05-28 20:14:22 +0800

    Add initial content

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,141 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>AJ Villalobos</h1>
+        <h2>
+          Full Stack Web Developer / Ruby on Rails / Product-Minded Engineer
+        </h2>
+        <p>Iloilo City, Philippines / hello@ajvillalobos.com / +639176760513</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+
+      <p>
+        Ruby on Rails developer with 10 years of experience building full-stack
+        web applications. I specialize in fast, maintainable systems with real
+        business impact. I focus on product thinking, clean architecture, and
+        delivering features that matter.
       </p>
+
+      <div>
+        <h3>Experiences</h3>
+
+        <ul>
+          <li>
+            <h4>Shiftcare / May 2024 - Present</h4>
+            <p>Ruby on Rails Developer</p>
+            <p>Australia (Remote)</p>
+            <p>
+              Build scalable, user-friendly features for aged care using Rails
+              APIs and VueJS, serving both customers and internal users.
+            </p>
+          </li>
+          <li>
+            <h4>Radiopaedia / June 2023 - Jan 2024</h4>
+            <p>Ruby on Rails Developer</p>
+            <p>Australia (Remote)</p>
+            <p>
+              Improved UX by making key pages mobile-friendly and redesigning
+              the layout using TypeScript, Preact, and Rails API integration.
+            </p>
+          </li>
+          <li>
+            <h4>Kangarootime / July 2019 - May 2023</h4>
+            <p>Senior Ruby on Rails Developer</p>
+            <p>USA (Remote)</p>
+            <p>
+              Led refactoring of a core Rails model, enabling new feature
+              development. Maintained legacy systems and built COVID-19 tools to
+              support childcare centers’ daily operations.
+            </p>
+          </li>
+          <li>
+            <h4>Bloom Solutions / Nov 2017 - Dec 2020</h4>
+          </li>
+          <li>
+            <h4>Tanda / Jun 2017 - Jan 2018</h4>
+          </li>
+          <li>
+            <h4>Proudcloud / Aug 2015 - Feb 2016</h4>
+          </li>
+          <li>
+            <h4>YouSource / Sep 2014 - Aug 2015</h4>
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <h3>Other Works</h3>
+
+        <ul>
+          <li>
+            <h4>Bruskoffee Admin</h4>
+            <p>
+              Inventory system built for a specialty coffee shop. Features
+              include automatic low-stock alerts, ingredient-based costing,
+              sales reporting, and 3rd-party POS + delivery report integration.
+            </p>
+            <div>
+              <span>ruby on rails 7</span>
+              <span>heroku</span>
+              <span>tailwind</span>
+              <span>api integration</span>
+              <span>avo admin</span>
+              <span>devise</span>
+              <span>solid queue</span>
+            </div>
+          </li>
+          <li>
+            <h4>Medical Laboratory Admin</h4>
+            <p>
+              Inventory and access control system for a medical lab. Tracks
+              perishable goods, sales, and stock movement with user
+              authorizations and audit history.
+            </p>
+            <div>
+              <span>ruby on rails 8</span>
+              <span>heroku</span>
+              <span>tailwind</span>
+              <span>devise</span>
+              <span>pundit</span>
+              <span>paper trail</span>
+            </div>
+          </li>
+          <li>
+            <h4>Privacy-focused Marketing Tool</h4>
+            <p>
+              A simple Bitly alternative focused on privacy and GDPR compliance.
+              Originally launched as a paid tool and later acquired by an early
+              user.
+            </p>
+            <div>
+              <span>ruby on rails 5</span>
+              <span>heroku</span>
+              <span>bootstrap</span>
+              <span>aws</span>
+              <span>paddle</span>
+              <span>postmark</span>
+              <span>subdomains</span>
+            </div>
+          </li>
+          <li>
+            <h4>Clover Ruby Gem</h4>
+            <p>
+              An open-source Ruby wrapper for a major POS system’s REST API.
+              Though now archived, the gem was widely forked and used in
+              production integrations.
+            </p>
+            <div>
+              <span>ruby</span>
+              <span>rubygems</span>
+              <span>api integration</span>
+            </div>
+          </li>
+        </ul>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
